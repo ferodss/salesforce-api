2bd96d9e740a5407379776f05cf23d867046309f
Felipe Rodrigues <felipe.rodrigues@rocket-internet.com.br>
2014-10-07 11:53:14 -0300

    Add more Client tests

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -15,24 +15,51 @@ class ClientTest extends \PHPUnit_Framework_TestCase
 
     public function testShouldPassHttpClientInterfaceToConstructor()
     {
-        $client = new Client($this->getWSDLPath(), $this->getHttpClientMock());
+        $client = $this->getClient();
 
         $this->assertInstanceOf('Salesforce\HttpClient\HttpClientInterface', $client->getHttpClient());
     }
 
     public function testShouldNotHaveToSetSoapClient()
     {
-        $client = new Client($this->getWSDLPath(), $this->getHttpClientMock());
+        $client = $this->getClient();
 
         $this->assertInstanceOf('Salesforce\Soap\SoapClientInterface', $client->getSoapClient());
     }
 
     public function testShouldBeAbletoSetSoapClient()
     {
-        $client = new Client($this->getWSDLPath(), $this->getHttpClientMock());
+        $client = $this->getClient();
         $client->setSoapClient($this->getSoapClientMock());
 
         $this->assertInstanceOf('Salesforce\Soap\SoapClientInterface', $client->getSoapClient());
+    }
+
+    /**
+     * @dataProvider apiObjectsDataProvider
+     */
+    public function testSouldBeAbleToGetApiObjects($apiName, $instanceOf)
+    {
+        $client = $this->getClient();
+        $api = $client->api($apiName);
+
+        $this->assertInstanceOf($instanceOf, $api);
+    }
+
+    public function apiObjectsDataProvider()
+    {
+        return [
+            ['bulk', 'Salesforce\Api\Bulk'],
+        ];
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function shouldThrowsExceptionOnGetInvalidApi()
+    {
+        $client = $this->getClient();
+        $api = $client->api('FooApi');
     }
 
     public function testShouldBeAuthenticatorProxy()
@@ -57,6 +84,39 @@ class ClientTest extends \PHPUnit_Framework_TestCase
         $client->setSoapClient($soapClient);
 
         $client->authenticate($login, $password, $token);
+    }
+
+    public function testShouldNotBeAuthenticated()
+    {
+        $client = $this->getClient();
+
+        $this->assertFalse($client->isAuthenticated());
+    }
+
+    public function testShouldBeAuthenticated()
+    {
+        $login    = 'foo@bar.com';
+        $password = '123foobar';
+        $token    = 'xpto321';
+
+        $httpClient = $this->getHttpClientMock();
+
+        $loginResult = $this->getLoginResultMock();
+        $loginResult->method('getServerUrl')
+            ->willReturn('https://cs21.salesforce.com/services/Soap/c/32.0/');
+
+        $soapClient = $this->getSoapClientMock();
+        $soapClient->expects($this->once())
+            ->method('authenticate')
+            ->with($login, $password, $token)
+            ->willReturn($loginResult);
+
+        $client = new Client($this->getWSDLPath(), $httpClient);
+        $client->setSoapClient($soapClient);
+
+        $client->authenticate($login, $password, $token);
+
+        $this->assertTrue($client->isAuthenticated());
     }
 
     public function getHttpClientMock()
@@ -95,6 +155,11 @@ class ClientTest extends \PHPUnit_Framework_TestCase
     public function getWSDLPath()
     {
         return __DIR__ . '/Fixtures/sandbox.enterprise.wsdl.xml';
+    }
+
+    protected function getClient()
+    {
+        return new Client($this->getWSDLPath(), $this->getHttpClientMock());
     }
 
 } 
